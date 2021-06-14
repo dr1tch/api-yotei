9c8d4b8fa87f9157d0302a6376392e0e12be657c
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2021-06-15 00:17:44 +0100

    adding the users app to the admin dash.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = ('email', 'username', 'name',)
+    # list_filter = ('id', 'logo', 'email', 'username', 'name',
+    #                'phone_number', 'is_active', 'is_staff', 'type')
+    ordering = ('-created_date',)
+    list_display = ('id', 'email', 'username', 'name', 
+                    'phone_number', 'is_active', 'is_staff', 'wilaya','role')
+
+
+admin.site.register(User, UserAdminConfig)
