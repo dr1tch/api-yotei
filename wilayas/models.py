ba4976d73c84d26558329bb88f3e2717e9e4c02f
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2021-06-14 23:23:49 +0100

    wilaya model created.

--- wilayas/models.py
+++ wilayas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Wilaya(models.Model):
+    name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    deleted_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'wilaya'
+        verbose_name_plural = 'wilayas'
+
+    def __str__(self):
+        return self.name
