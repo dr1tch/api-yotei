fff6724bef2ef1e78f7e50d8405111bdae8422da
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2021-06-15 10:27:43 +0100

    categories app finished.

---
+++ categories/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    deleted_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
