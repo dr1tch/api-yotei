b4445615af9ff8267f94dc60ae115ecdd8966b61
Youssouf Kacemi <youssouf.kacemi@gmail.com>
2021-06-27 02:57:04 +0100

    feedbacks app finished.

---
+++ feedbacks/models.py
@@ -0,0 +1,40 @@
+from users.models import User
+from services.models import Service
+import datetime
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+from django.db import models
+
+# Create your models here.
+
+
+class Feedback(models.Model):
+
+    # 0 : CLIENT ===> SERVICE
+    # 1 : SERVICE ===> CLIENT
+    class Types(models.IntegerChoices):
+        CLIENT = 0
+        SERVICE = 1
+
+    base_type = Types.CLIENT
+    client = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='client_feedback')
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name='service_feedback')
+    type = models.IntegerField(
+        _("Type"), choices=Types.choices, default=base_type
+    )
+    rating = models.FloatField(blank=False)
+    body = models.TextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+    deleted_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'feedback'
+        verbose_name_plural = 'feedbacks'
+
+    def __str__(self):
+        return self.body
---
+++ feedbacks/serializers.py
@@ -0,0 +1,9 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Feedback
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = '__all__'
